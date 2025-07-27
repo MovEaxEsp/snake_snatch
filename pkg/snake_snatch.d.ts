a316c7d259d427a175adab38682b4ad3ff48ddd3
Pawel Grzech <pgrzech1@bloomberg.net>
2025-07-27 07:28:19 -0400

    Add pkg

---
+++ pkg/snake_snatch.d.ts
@@ -0,0 +1,56 @@
+/* tslint:disable */
+/* eslint-disable */
+export function init_state(config: any, canvas: any, _images: any, _audio_ctx: any, _sounds: any): void;
+export function run_frame(): void;
+export function handle_mouse_event(event: any): void;
+export function default_config(): any;
+export function update_config(config: any): void;
+export function be_host(): void;
+export function be_client(): void;
+export function ping_connections(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly init_state: (a: any, b: any, c: any, d: any, e: any) => void;
+  readonly run_frame: () => void;
+  readonly handle_mouse_event: (a: any) => void;
+  readonly default_config: () => any;
+  readonly update_config: (a: any) => void;
+  readonly be_host: () => void;
+  readonly be_client: () => void;
+  readonly ping_connections: () => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_4: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly closure104_externref_shim: (a: number, b: number, c: any) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h23b68e02e3e2c5b9: (a: number, b: number, c: number, d: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6c8d9cf9338aef13: (a: number, b: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
